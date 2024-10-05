f692f18daf32070294850de3c906c6d6bd6935c9
JDkinior <100943713+JDkinior@users.noreply.github.com>
2024-10-05 01:48:00 +0000

    versionando a anterior

--- functions/register.js
+++ functions/register.js
@@ -1,16 +1,6 @@
-const sqlite3 = require('sqlite3').verbose();
 const { v4: uuidv4 } = require('uuid');
 
-const db = new sqlite3.Database('./ecommerce.db');
-
 exports.handler = async (event, context) => {
-    if (event.httpMethod !== 'POST') {
-        return {
-            statusCode: 405,
-            body: JSON.stringify({ message: 'Method Not Allowed' })
-        };
-    }
-
     const data = JSON.parse(event.body);
 
     const user = {
@@ -20,21 +10,10 @@ exports.handler = async (event, context) => {
         password: data.password // En un proyecto real, asegúrate de hashear la contraseña
     };
 
-    return new Promise((resolve, reject) => {
-        const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-        db.run(query, [user.username, user.email, user.password], function (error) {
-            if (error) {
-                console.error('Error al registrar usuario:', error);
-                reject({
-                    statusCode: 500,
-                    body: JSON.stringify({ message: 'Error al registrar usuario', error })
-                });
-            } else {
-                resolve({
-                    statusCode: 200,
-                    body: JSON.stringify({ message: 'Usuario registrado con éxito', user })
-                });
-            }
-        });
-    });
+    // Aquí puedes agregar la lógica para guardar el usuario en una base de datos
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Usuario registrado con éxito', user })
+    };
 };
